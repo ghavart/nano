7d8936bb9802b587be6982229c6d268493c97a07
Art <ghavart@gmail.com>
2020-02-06 16:40:19 -0800

    Add a simple opencv imshow
    Add a simple C++ binary that creates a Gstreamer pipeline
    from the CSI camera and shows the output.
    
    compiled on Nano with:
    g++ opencv_camera_simple_out.cc -o main

---
+++ opencv_camera_simple_out.cc
@@ -0,0 +1,25 @@
+// By Art Gavrysh: ghavart@gmail.com (2020)
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+    Mat frame;
+    VideoCapture cap;
+    cap.open("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1980,format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
+    if (!cap.isOpened()) {
+        std::cerr << "Cannot open thye video0 resource. Exiting" << std::endl;
+        return 0;
+    }
+    while (1) {
+        cap >> frame;
+        if (frame.empty()) {
+            break;
+        }
+        imshow("cam", frame);
+        waitKey(20);
+    }
+    cap.release();
+    return 0;
+}
